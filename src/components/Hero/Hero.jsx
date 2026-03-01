441b79685778f750ee343683c41b1d3d84bcabd7
Haris Shahnawaz <harisshahnawaz97@gmail.com>
2026-03-02 01:11:24 +0500

    make changes in our hero section and add some buttons and images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -21,10 +26,32 @@ const Hero = () => {
              </div>
        </div>
    {  /* figures */}
-
+     <div className="figures">
+        <div><span>+140</span><span>Expert Coaches</span></div>
+        <div><span>+978</span><span>Members joined</span></div>
+        <div><span>+50</span><span>Fitness Programs</span></div>
+        
+     </div>
       
+   {  /* hero buttons */}
+   <div className="hero-buttons">
+    <buttons
+    className="btn">Get Started</buttons>
+    <buttons className="btn">Learn More</buttons>
+   </div>
     </div>
-     <div className="right-h">right side</div>
+     <div className="right-h">
+        <button className='btn'>Join Now</button>
+
+        <div className="heart-rate">
+            <img src={Heart} alt="" />
+            <span>Heart Rate</span><span>113 bpm</span>
+        </div>
+   {  /* hero images*/}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+        
+        </div>
     </div>
   )
 }
