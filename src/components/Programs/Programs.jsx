cc93ffaf05265f41c8550e5e6289c995fe29c63c
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-03 00:38:32 +0500

    add new file

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import './Programs.css'
 const Programs = () => {
   return (
-    <div>
-      programs
+   <div className="Programs" id="programs">
+    {/*header*/}
+    <div className="programs-header">
+      <span className='stroke-text'>Explore our</span>
+      <span>Programs</span>
+      <span className='stroke-text'>to shape you</span>
     </div>
+   </div>
   )
 }
 
