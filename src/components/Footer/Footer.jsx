fa4a1c68fedd1887270c462a19da2413802b7560
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-04 03:51:01 +0500

    added blur effects

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Footer.css'
+import Github from '../../assets/github.png'
+import Instagram from '../../assets/instagram.png'
+import LinkedIn from '../../assets/linkedin.png'
+import Logo from '../../assets/logo.png'
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <div className="footer-container">
+  <hr />
+
+  <div className="footer">
+
+    <div className="social-links">
+      <img src={Github} alt="github" />
+      <img src={Instagram} alt="instagram" />
+      <img src={LinkedIn} alt="linkedin" />
+    </div>
+
+    <div className="logo">
+      <img src={Logo} alt="logo" />
+    </div>
+     <div className="blur blur-f1"></div>
+     <div className="blur blur-f2"></div>
+
+     
+
+    <p className="copyright">
+      © 2026 FitClub. All rights reserved.
+    </p>
+
+  </div>
+</div>
   )
 }
-
 export default Footer
